e64b0923f91c22ca905b65e71765faaa04b08110
qinxiaokang <qinxiaokang@ppdai.com>
2017-05-27 16:26:21 +0800

    删除代码，测试pod search PPDBLUtils

--- PPDBLUtils/PPDBLUtils.h
+++ PPDBLUtils/PPDBLUtils.h
@@ -12,27 +12,6 @@
 FOUNDATION_EXPORT double PPDBLUtilsVersionNumber;
 FOUNDATION_EXPORT const unsigned char PPDBLUtilsVersionString[];
 
-#import <PPDBLUtils/NSArray+PPDBLHelper.h>
-#import <PPDBLUtils/NSDate+PPDBLHelper.h>
-#import <PPDBLUtils/NSDateFormatter+PPDBLHelper.h>
-#import <PPDBLUtils/NSDictionary+PPDBLHelper.h>
-#import <PPDBLUtils/NSString+PPDBLHelper.h>
-#import <PPDBLUtils/NSTimer+PPDBLHelper.h>
-#import <PPDBLUtils/UIColor+PPDBLHelper.h>
-#import <PPDBLUtils/UIImage+PPDBLHelper.h>
-#import <PPDBLUtils/UIView+PPDBLHelper.h>
-
-#else
-
-#import "NSArray+PPDBLHelper.h"
-#import "NSDate+PPDBLHelper.h"
-#import "NSDateFormatter+PPDBLHelper.h"
-#import "NSDictionary+PPDBLHelper.h"
-#import "NSString+PPDBLHelper.h"
-#import "NSTimer+PPDBLHelper.h"
-#import "UIColor+PPDBLHelper.h"
-#import "UIImage+PPDBLHelper.h"
-#import "UIView+PPDBLHelper.h"
 
 #endif
 
